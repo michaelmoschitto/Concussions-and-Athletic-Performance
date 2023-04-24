8e10a5478fa05b7c0ea3c9c83f2b5fbad920ea16
michaelmoschitto <mikemoschitto@gmail.com>
2023-04-23 20:21:12 -0700

    pytorch w/ sklearn randomsearch proof of concept

--- install_kernel.sh
+++ install_kernel.sh
@@ -2,5 +2,5 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install ipykernel
-ipython kernel install --user --name=kernel
+ipython kernel install --user --name=kernel2
 pip install -r requirements.txt
